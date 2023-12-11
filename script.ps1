73f879cf1b5d070232e7efcf78030aaceb5b8e78
MatthisDoney <matthis.doney@outlook.fr>
2023-12-11 11:00:13 +0100

    importation des bibliotheques

---
+++ script.ps1
@@ -0,0 +1,8 @@
+ symfony composer require symfony/form
+ symfony composer require symfony/validator
+ symfony composer require symfony/orm-pack
+ symfony composer require symfony/mime
+ symfony composer require symfony/asset
+ symfony composer require symfony/mailer
+ symfony composer require sensio/framework-extra-bundle
+ symfony composer require symfony/security-bundle
